48620b1b2a593d9a2bed12ebae75127738ab5392
lukas <willmann.Lukas@web.de>
2023-05-19 11:25:18 +0200

    Added some Sample Scenes and Prefabs

---
+++ Prototyp/Assets/README.cs
@@ -0,0 +1,9 @@
+/*
+ * README
+ *  
+ *  This project is the practice project for the Game Productions course in the second part of summer semester 23.
+ *  In the folder Paulander Spezis you will find all the folders on which the group members of the Paulander Spezis team are actively working. 
+ *  The top level asset folders of this project should not be edited as they contain sample assets and libraries.
+ *  Changes to prefabs should be made within the prefab and not within the scene in which the prefab was placed.
+ * 
+ */
