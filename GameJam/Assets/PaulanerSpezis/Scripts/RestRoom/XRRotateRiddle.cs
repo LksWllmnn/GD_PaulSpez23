530fd74b2b41c5d846820bddbe418d336d0c9b3f
lukas <willmann.Lukas@web.de>
2023-07-18 19:38:35 +0200

    rotating cube

--- GameJam/Assets/PaulanerSpezis/Scripts/RestRoom/XRRotateRiddle.cs
+++ GameJam/Assets/PaulanerSpezis/Scripts/RestRoom/XRRotateRiddle.cs
@@ -31,13 +31,37 @@ public class XRRotateRiddle : MonoBehaviour
         if (_isClicked)
         {
             Vector3 _difference = _interActorTransform.position - transform.position;
-            if (_difference.x < 0)
+            if (Mathf.Abs(_difference.x) < Mathf.Abs(_difference.z) && _difference.z > 0) // north
             {
-                transform.Rotate(new Vector3(-(oldPosition.y - _interActorTransform.localPosition.y) * 10, 0, 0), Space.World);
+                Debug.Log("North");
+                transform.Rotate(new Vector3(
+                    (oldPosition.y - _interActorTransform.localPosition.y) * 30000 * Time.deltaTime, //vertical
+                    -(oldPosition.x - _interActorTransform.localPosition.x) * 30000 * Time.deltaTime, 0),//horizontal 
+                    Space.World);
                 
-            } else if (_difference.x > 0)
+            } else if (Mathf.Abs(_difference.x) > Mathf.Abs(_difference.z) && _difference.x > 0) // east
             {
-                transform.Rotate(new Vector3((oldPosition.y - _interActorTransform.localPosition.y) * 10, 0, 0), Space.World);
+                transform.Rotate(new Vector3(
+                    0, 
+                    (oldPosition.z - _interActorTransform.localPosition.z) * 30000 * Time.deltaTime, //horizontal
+                    -(oldPosition.y - _interActorTransform.localPosition.y) * 30000 * Time.deltaTime), Space.World); //vertical
+                Debug.Log("East");
+            }
+            else if (Mathf.Abs(_difference.x) < Mathf.Abs(_difference.z) && _difference.z < 0) // south
+            {
+                transform.Rotate(new Vector3(
+                    -(oldPosition.y - _interActorTransform.localPosition.y) * 30000 * Time.deltaTime, //vertical
+                    (oldPosition.x - _interActorTransform.localPosition.x) * 30000 * Time.deltaTime, //horizontal
+                    0), Space.World);
+                Debug.Log("South");
+            }
+            else if (Mathf.Abs(_difference.x) > Mathf.Abs(_difference.z) && _difference.x < 0) // west
+            {
+                transform.Rotate(new Vector3(
+                    0, 
+                    -(oldPosition.z - _interActorTransform.localPosition.z) * 30000 * Time.deltaTime, //horizontal
+                    (oldPosition.y - _interActorTransform.localPosition.y) * 30000 * Time.deltaTime), Space.World); //vertical
+                Debug.Log("West");
             }
             oldPosition = _interActorTransform.localPosition;
             Debug.Log(_difference.x);
